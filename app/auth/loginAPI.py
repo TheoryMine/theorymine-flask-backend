7d84c0eeec72276ca2ca247574af3117f3f6d1e6
Flaminia <flaminic@thoughtworks.com>
2018-11-16 16:31:59 -0300

    change login body response

--- app/auth/loginAPI.py
+++ app/auth/loginAPI.py
@@ -19,26 +19,25 @@ class LoginAPI(MethodView):
         try:
             request_body = request.get_json()
             verify_login_request_body(request_body, self.logger)
-            user_id = self.all_users.fetch_one_by_email_and_password(request_body['email'],request_body['password'])
-            auth_token = self.user_token.encode_auth_token(user_id)
-            if not user_id:
+            user = self.all_users.fetch_one_by_email_and_password(request_body['email'],request_body['password'])
+            if not user:
                 raise NonExistentError('Password and Email do not match')
+            user_id = user['id']
+            auth_token = self.user_token.encode_auth_token(user_id)
             response_object = {
-                'status': 'success',
-                'message': 'Successfully logged in.',
                 'user_id' : user_id,
                 'auth_token': auth_token.decode()
             }
             return make_response(jsonify(response_object)), 200
         except BadRequestError as e:
             self.logger.error('ERR_BAD_REQUEST EXCEPTION: ' + str(e))
-            resp = jsonify({'status': 'fail', 'code': 'ERR_BAD_REQUEST', 'message': e.message})
+            resp = jsonify({'code': 'ERR_BAD_REQUEST', 'message': e.message})
             return make_response(resp), 400
         except NonExistentError as e:
             self.logger.error('ERR_UNAUTHORISED EXCEPTION: ' + str(e))
-            resp = jsonify({'status': 'fail', 'code': 'ERR_UNAUTHORISED', 'message': e.message})
+            resp = jsonify({'code': 'ERR_UNAUTHORISED', 'message': e.message})
             return make_response(resp), 401
         except Exception as e:
             self.logger.error('UNKNOWN EXCEPTION: ' + str(e))
-            resp = jsonify({'status': 'fail', 'code': 'ERR_UNKNOWN', 'message': 'unknown error'})
+            resp = jsonify({'code': 'ERR_UNKNOWN', 'message': 'unknown error'})
             return make_response(resp), 500
--- app/auth/usersAPI.py
+++ app/auth/usersAPI.py
@@ -26,17 +26,15 @@ class UsersAPI(MethodView):
             auth_token = self.user_token.encode_auth_token(user_id)
 
             response_object = {
-                'status': 'success',
-                'message': 'Successfully registered.',
                 'auth_token': auth_token.decode(),
                 'user_id': user_id
             }
             return make_response(jsonify(response_object)), 201
         except BadRequestError as e:
-            resp = jsonify({'status': 'fail', 'code': 'ERR_BAD_REQUEST', 'message': e.message})
+            resp = jsonify({'code': 'ERR_BAD_REQUEST', 'message': e.message})
             self.logger.error('ERR_BAD_REQUEST EXCEPTION: ' + str(e))
             return make_response(resp), 400
         except Exception as e:
             self.logger.error('UNKNOWN EXCEPTION: ' + str(e))
-            resp = jsonify({'status': 'fail', 'code': 'ERR_UNKNOWN', 'message': 'unknown error'})
+            resp = jsonify({'code': 'ERR_UNKNOWN', 'message': 'unknown error'})
             return make_response(resp), 500
