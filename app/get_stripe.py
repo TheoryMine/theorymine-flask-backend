59cf7586491b5495e6ea68e2cc617ee5fd8a266f
Flaminia <flaminic@thoughtworks.com>
2018-11-17 16:01:56 -0300

    fix tests for post orders

--- app/get_stripe.py
+++ app/get_stripe.py
@@ -7,12 +7,13 @@ def get_stripe():
     current_app.logger.info(current_app.config['ENV'])
     if 'stripe' not in g:
         if current_app.config['ENV'] == 'test':
-            stripe_client = FakeStripe()
+            stripe_charge_client = FakeStripe()
         else:
             stripe_client = stripe
             secret_key = current_app.config['STRIPE_SECRET_KEY']
             stripe_client.api_key = secret_key
-        g.stripe = stripe_client
+            stripe_charge_client = stripe_client.Charge
+        g.stripe = stripe_charge_client
     return g.stripe
 
 def init_app(app):
@@ -21,22 +22,17 @@ def init_app(app):
 
 class FakeStripe:
     def __init__(self):
-        self.Charge = Charge()
+        self.stripe_calls = None
 
-
-class Charge:
-    def __init__(self):
-        self.calls = []
-
-    def create(self, customer, amount, currency, description, fail=False):
+    def create(self, source, amount, currency, description, fail=False):
         if fail:
             raise error.CardError('This is a fake error')
         else:
 
-            call_params = {'customer': customer, 'amount': amount,
+            call_params = {'source': source, 'amount': amount,
                            'currency': currency, 'description': description, }
-            self.calls.append(call_params)
+            self.stripe_calls = [1]
             return {'status': 'fake stripe called', **call_params}
 
     def get_calls(self):
-        return self.calls
+        return self.stripe_calls
