0f0593eebf3af185d3c2cb7a625468c7e80aaafc
Flaminia <flaminic@thoughtworks.com>
2018-11-21 13:07:14 -0300

    remove unused config

--- app/config.py
+++ app/config.py
@@ -9,8 +9,6 @@ class BaseConfig(object):
     DATABASE_USER = os.getenv("DATABASE_USER")
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
     DATABASE_NAME = os.getenv("DATABASE_NAME")
-    BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
-    BASIC_AUTH_PASSWORD  =  os.getenv("BASIC_AUTH_PASSWORD")
     SECRET_KEY = os.getenv('SECRET_KEY')
     SESSION_LENGTH = datetime.timedelta(days=0, hours=2)
     STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
@@ -26,8 +24,6 @@ class DevelopmentConfig(BaseConfig):
     DATABASE_USER = os.getenv("DATABASE_USER") or "root"
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD") or ""
     DATABASE_NAME = os.getenv("DATABASE_NAME") or "theorymine"
-    BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
-    BASIC_AUTH_PASSWORD  =  os.getenv("BASIC_AUTH_PASSWORD")
     SECRET_KEY = os.getenv('SECRET_KEY') or "\xe4z\xedW\x95\xfa^RX\x87\xc3\xcd$\x9e\xc5\x86\x9a\x0f\x8f\x8a]\xa9*\x8a"
     SESSION_LENGTH = datetime.timedelta(days=1)
     STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
@@ -41,8 +37,6 @@ class TestingConfig(BaseConfig):
     DATABASE_USER = os.getenv("DATABASE_USER") or "root"
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD") or ""
     DATABASE_NAME = os.getenv("DATABASE_NAME") or "theorymine-test"
-    BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
-    BASIC_AUTH_PASSWORD  =  os.getenv("BASIC_AUTH_PASSWORD")
     SECRET_KEY = os.getenv('SECRET_KEY') or "\xe4z\xedW\x95\xfa^RX\x87\xc3\xcd$\x9e\xc5\x86\x9a\x0f\x8f\x8a]\xa9*\x8a"
     SESSION_LENGTH = datetime.timedelta(days=0, seconds =1)
     STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
